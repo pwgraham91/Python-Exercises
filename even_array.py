52f81a52e46ea12f9d634170c1e8160191d2c6a2
Peter Graham <pwgraham91@gmail.com>
2018-04-16 11:14:07 -0500

    even array improvement

--- even_array.py
+++ even_array.py
@@ -12,12 +12,28 @@ An empty array should be treated like a 0 in this problem.
 """
 
 
-def find_even_index(input_array):
+def find_even_index_simple(input_array):
     for counter, i in enumerate(input_array):
         if sum(input_array[:counter]) == sum(input_array[counter + 1:]):
             return counter
 
 
-assert find_even_index([1, 2, 3, 4, 3, 2, 1]) == 3
-assert find_even_index([1, 100, 50, -51, 1, 1]) == 1
-assert find_even_index([20, 10, -80, 10, 10, 15, 35]) == 0
+assert find_even_index_simple([1, 2, 3, 4, 3, 2, 1]) == 3
+assert find_even_index_simple([1, 100, 50, -51, 1, 1]) == 1
+assert find_even_index_simple([20, 10, -80, 10, 10, 15, 35]) == 0
+
+
+def find_even_index_performant(input_array):
+    left_side_sum = 0
+    right_side_sum = sum(input_array)
+
+    for counter, i in enumerate(input_array):
+        right_side_sum -= i
+        if left_side_sum == right_side_sum:
+            return counter
+        left_side_sum += i
+
+
+assert find_even_index_performant([1, 2, 3, 4, 3, 2, 1]) == 3
+assert find_even_index_performant([1, 100, 50, -51, 1, 1]) == 1
+assert find_even_index_performant([20, 10, -80, 10, 10, 15, 35]) == 0

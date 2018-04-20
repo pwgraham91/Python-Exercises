8e1c8485e0ca53fed5f2aff21a2c4294ce0404b7
Peter Graham <pwgraham91@gmail.com>
2018-04-19 22:35:01 -0500

    whos faster

--- list_filtering.py
+++ list_filtering.py
@@ -15,9 +15,9 @@ import datetime
 
 start = datetime.datetime.now()
 
-assert filter_list_comprehension([1, 2, 'a', 'b']) == [1, 2]
-assert filter_list_comprehension([1, 'a', 'b', 0, 15]) == [1, 0, 15]
-assert filter_list_comprehension([1, 2, 'aasf', '1', '123', 123]) == [1, 2, 123]
+assert filter_list([1, 2, 'a', 'b']) == [1, 2]
+assert filter_list([1, 'a', 'b', 0, 15]) == [1, 0, 15]
+assert filter_list([1, 2, 'aasf', '1', '123', 123]) == [1, 2, 123]
 
 stop = datetime.datetime.now()
 print(stop - start)
@@ -25,9 +25,11 @@ print(stop - start)
 
 start = datetime.datetime.now()
 
-assert filter_list([1, 2, 'a', 'b']) == [1, 2]
-assert filter_list([1, 'a', 'b', 0, 15]) == [1, 0, 15]
-assert filter_list([1, 2, 'aasf', '1', '123', 123]) == [1, 2, 123]
+assert filter_list_comprehension([1, 2, 'a', 'b']) == [1, 2]
+assert filter_list_comprehension([1, 'a', 'b', 0, 15]) == [1, 0, 15]
+assert filter_list_comprehension([1, 2, 'aasf', '1', '123', 123]) == [1, 2, 123]
 
 stop = datetime.datetime.now()
 print(stop - start)
+
+# in case you're wondering, the comprehensions appear to be faster

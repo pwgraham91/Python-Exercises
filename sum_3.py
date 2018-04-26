ac398c490c3c9b451ce4cd9c5dc24ef7b95597a3
Peter Graham <pwgraham91@gmail.com>
2018-04-26 11:15:19 -0500

    sum 3

--- sum_3.py
+++ sum_3.py
@@ -11,13 +11,24 @@ If any set of 3 elements is equal to the constant c, then return false
 
 """
 
-def find_3(array):
 
-    for count, i in enumerate(array):
-        pass
+def find_3(array, constant):
+
+    array_len = len(array)
+    for count, a in enumerate(array):
+        if (array_len < count + 3) or count >= array_len - 2:
+            break
+
+        for b in range(array_len - count - 1):
+            for c in range(array_len - count - b - 2):
+                b_obj = array[count + b + 1]
+                c_obj = array[count + b + c + 2]
+                current_sum = a + b_obj + c_obj
+                if current_sum == constant:
+                    return True
+    return False
 
 
-
-assert find_3([6, 2, 3, 4])
-assert find_3([6, 2, 3, 4, 5, 5, 5])
-assert not find_3([1, 1, 1, 1, 1])
+assert find_3([6, 2, 3, 4], 9)
+assert find_3([6, 2, 3, 4, 5, 8, 9], 15)
+assert not find_3([1, 1, 1, 1, 1], 2)

1685f318cc3d0e1871521bd15277475e8c40e8ec
Peter Graham <pwgraham91@gmail.com>
2018-04-27 10:24:16 -0500

    WIP better summation

--- sum_3.py
+++ sum_3.py
@@ -13,6 +13,7 @@ If any set of 3 elements is equal to the constant c, then return false
 
 
 def find_3(array, constant):
+    """ O(n^3) """
 
     array_len = len(array)
     for count, a in enumerate(array):
@@ -29,6 +30,37 @@ def find_3(array, constant):
     return False
 
 
+def find_3_dict(array, constant):
+    """ O(n^2) """
+
+    mapped_ints = {}
+    for i in array:
+        if mapped_ints.get(i):
+            mapped_ints[i] += 1
+        else:
+            mapped_ints[i] = 1
+
+    array_len = len(array)
+    for count, a in enumerate(array):
+        if (array_len < count + 3) or count >= array_len - 2:
+            break
+
+        for b in range(array_len - count - 1):
+            b_obj = array[count + b + 1]
+            needed_number = constant - a - b_obj
+
+            number_frequency = mapped_ints.get(needed_number, 0)
+            if number_frequency >= 1 + (1 if needed_number == a else 0) + (1 if needed_number == b_obj else 0):
+                return True
+    return False
+
+
+
 assert find_3([6, 2, 3, 4], 9)
 assert find_3([6, 2, 3, 4, 5, 8, 9], 15)
 assert not find_3([1, 1, 1, 1, 1], 2)
+
+assert find_3_dict([6, 2, 3, 4], 9)
+assert not find_3_dict([1, 1, 1, 1, 1], 2)
+assert find_3_dict([6, 2, 3, 4, 5, 8, 9], 15)
+assert find_3_dict([-3, -2, -2, -1, 0, 4, -2, 4], 6)

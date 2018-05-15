b56e563142e55c3871226249a1be562923dfb032
Peter Graham <pwgraham91@gmail.com>
2018-05-15 15:45:55 -0500

    improve validator

--- sudoku.py
+++ sudoku.py
@@ -138,10 +138,12 @@ def validate_sudoku_matrix(matrix):
 
     for i in range(9):
         # validate columns
-        validate_group([row[i] for row in matrix])
+        if not validate_group([row[i] for row in matrix]):
+            return False
+    return True
 
 
-validate_sudoku_matrix(solve([
+assert validate_sudoku_matrix(solve([
     [None, None, None, 2, 6, None, 7, None, 1],
     [6, 8, None, None, 7, None, None, 9, None],
     [1, 9, None, None, None, 4, 5, None, None],
@@ -154,7 +156,7 @@ validate_sudoku_matrix(solve([
 ]))
 
 
-validate_sudoku_matrix(solve([
+assert validate_sudoku_matrix(solve([
     [4, None, 5, None, 2, None, None, 6, None],
     [6, None, None, 9, None, None, None, 1, 3],
     [None, 9, None, 3, 6, 7, None, None, 5],
@@ -164,4 +166,16 @@ validate_sudoku_matrix(solve([
     [8, None, None, 2, 4, 5, None, 9, None],
     [9, 7, None, None, None, 3, None, None, 2],
     [None, 5, None, None, 7, None, 8, None, 6]
+]))
+
+assert validate_sudoku_matrix(solve([
+    [None, 8, None, None, None, 6, None, 2, 5],
+    [None, 1, 5, None, 7, None, None, None, None],
+    [None, None, None, None, None, None, 6, None, None],
+    [None, 5, None, None, None, 1, None, None, 3],
+    [1, None, None, 8, None, 3, None, None, 7],
+    [9, None, None, 5, None, None, None, 6, None],
+    [None, None, 4, None, None, None, None, None, None],
+    [None, None, None, None, 6, None, 2, 8, None],
+    [5, 6, None, 4, None, None, None, 3, None]
 ]))

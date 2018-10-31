8de7d96f56129eb21c602cdb4082462b95b24f12
Peter Graham <pwgraham91@gmail.com>
2018-10-31 12:47:00 -0500

    singly linked list n

--- singly_linked_list_kth_last_node.py
+++ singly_linked_list_kth_last_node.py
@@ -1,48 +1,51 @@
+"""
+Write a function kth_to_last_node() that takes an integer kk and the head_node of a singly-linked list, and returns the
+kth to last node in the list.
+
+time complexity: n
+space complexity: k
+"""
 import unittest
 
 
-"""
-Write a function kth_to_last_node() that takes an integer kk and the head_node of a linked list, and returns the 
-kth to last node in the list.
-
-time complexity: n + k
-space complexity: 1
-"""
-
-
-class LinkedListNode:
+class SinglyLinkedListNode:
 
     def __init__(self, value):
         self.value = value
         self.next = None
-        self.prev = None
 
 
 def kth_to_last_node(k, base_node):
+    nodes = [base_node]
+
     current_node = base_node
+    nodes_size_has_reached_k = False
     while True:
         if current_node.next:
-            current_node.next.prev = current_node
             current_node = current_node.next
+
+            nodes.append(current_node)
+
+            if nodes_size_has_reached_k or len(nodes) > k:
+                nodes_size_has_reached_k = True
+                # reduce size of nodes because we only need to look back k times
+                nodes.pop(0)
         else:
             break
 
-    for i in range(k - 1):
-        if current_node.prev:
-            current_node = current_node.prev
-        else:
-            return
+    if k > len(nodes):
+        return
 
-    return current_node
+    return nodes[0]
 
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.a = LinkedListNode("Angel Food")
-        self.b = LinkedListNode("Bundt")
-        self.c = LinkedListNode("Cheese")
-        self.d = LinkedListNode("Devil's Food")
-        self.e = LinkedListNode("Eccles")
+        self.a = SinglyLinkedListNode("Angel Food")
+        self.b = SinglyLinkedListNode("Bundt")
+        self.c = SinglyLinkedListNode("Cheese")
+        self.d = SinglyLinkedListNode("Devil's Food")
+        self.e = SinglyLinkedListNode("Eccles")
 
         self.a.next = self.b
         self.b.next = self.c
